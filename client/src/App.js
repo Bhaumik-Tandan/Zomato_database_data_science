b912d2d81812ef2626baa46d731fe7653841dc1f
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-12-03 18:59:01 +0530

    cusisines.sav name changed

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from './form/form';
+function App() {
+
+  return (
+    <div>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/form/form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useForm } from 'react-hook-form';
+function Form() {
+    return (
+        <div>
+            sfa
+        </div>
+    )
+}
+
+export default Form;
